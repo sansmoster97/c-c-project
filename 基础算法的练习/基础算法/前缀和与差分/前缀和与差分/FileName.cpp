a7950f8e4273b05843ae43e58eefcc0908a0d30c
sansmoster97 <2796376863@qq.com>
2024-03-20 23:53:10 +0800

    qwq

---
+++ 基础算法的练习/基础算法/前缀和与差分/前缀和与差分/FileName.cpp
@@ -0,0 +1,13 @@
+#define  _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+/*
+前缀和的原理与特点
+
+
+*/
+int main() {
+	ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+
+	return 0;
+}
