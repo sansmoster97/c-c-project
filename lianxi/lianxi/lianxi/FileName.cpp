566eeed3344bc3b5c2d6c8a2aa0c1c531abd4da8
sansmoster97 <2796376863@qq.com>
2024-03-30 23:16:56 +0800

    1212

--- lianxi/lianxi/lianxi/FileName.cpp
+++ lianxi/lianxi/lianxi/FileName.cpp
@@ -1,24 +1,13 @@
-#define  _CRT_SECURE_NO_WARNINGS
+﻿#define  _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-#include <algorithm>
-#include <vector>
-constexpr auto endl = '\n';;
-
 using namespace std;
-int main() {
-	vector<int> a = { 5,3,1,4,5,3,6,8,8,2,3,3 };
-	int i = 0;
-	for (auto& i : a) cout << i << ' ';
-	sort(a.begin(), a.end());
-	cout << '\n';
-	for (auto& i : a) cout << i << ' ';
-	reverse(a.begin(), a.end());
-	cout << '\n';
-	for (auto& i : a) cout << i << ' ';
-	auto it = unique(a.begin(), a.end());
-	a.erase(it, a.end());
 
-	cout << '\n';
-	for (auto& i : a) cout << i << ' ';
+int main()
+{
+	int n, m;
+	cin >> n >> m;
+	int sum = 0;
+	sum = 10 * n + 9;
+	cout << sum/19;
 	return 0;
 }
